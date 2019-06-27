49d204eb945b90f2179fb44aa9eb291ac1515b8b
pengmengnanlan <46839374+pengmengnanlan@users.noreply.github.com>
2019-06-27 16:23:44 +0800

    test

--- test.py
+++ test.py
@@ -39,6 +39,7 @@ h = im.size[1]
 print("width：%s" % (w))
 print("height：%s" % (h))'''
 
+# download the radios
 '''user_agent = r'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 headers = {'User-Agent': user_agent}
 filename = r'D:\p\crawler\code.mp3'
@@ -46,8 +47,8 @@ urllib.request.urlretrieve(
     url="https://cf-media.sndcdn.com/bA1dsXOxA0j5.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vYkExZHNYT3hBMGo1LjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NjE0NTYzNTR9fX1dfQ__&Signature=RBzbQppVuOndaNdUvCm5mDgj7WtInMIVHyANURaIiMMxxfAe~BuZnGe2wmqdqEt~sSgOsqnhHD7wQNT2oUQv60B8Znb9YnZ8-0knutkGH2~e4Z2BbGdeKz1fDdLMvDRH5Qq7z7cOo39cjNXVppjEM-sQy8Ip-E6sjuXP10ssTnCpIcydNY1OdJFLY6pxlhTKjmML2hKaJxpyFnXchVYV71lg8us-QTySswmT4qS4nM~NSDj~rJZWRUm9nh9RVTyok~DIhC4maAij9i9zCMmTuQeFJGS6Yyd5eWE5CsPRfs2FBEolE0V7fH01r~MqMAr0hHw7anh-16LqCArEzWyCgA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ",
     filename=None, reporthook=None, data=(filename, headers))'''
 
-url = "https://cf-media.sndcdn.com/bA1dsXOxA0j5.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vYkExZHNYT3hBMGo1LjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NjE0NTYzNTR9fX1dfQ__&Signature=RBzbQppVuOndaNdUvCm5mDgj7WtInMIVHyANURaIiMMxxfAe~BuZnGe2wmqdqEt~sSgOsqnhHD7wQNT2oUQv60B8Znb9YnZ8-0knutkGH2~e4Z2BbGdeKz1fDdLMvDRH5Qq7z7cOo39cjNXVppjEM-sQy8Ip-E6sjuXP10ssTnCpIcydNY1OdJFLY6pxlhTKjmML2hKaJxpyFnXchVYV71lg8us-QTySswmT4qS4nM~NSDj~rJZWRUm9nh9RVTyok~DIhC4maAij9i9zCMmTuQeFJGS6Yyd5eWE5CsPRfs2FBEolE0V7fH01r~MqMAr0hHw7anh-16LqCArEzWyCgA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ"
+'''url = "https://cf-media.sndcdn.com/bA1dsXOxA0j5.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vYkExZHNYT3hBMGo1LjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NjE0NTYzNTR9fX1dfQ__&Signature=RBzbQppVuOndaNdUvCm5mDgj7WtInMIVHyANURaIiMMxxfAe~BuZnGe2wmqdqEt~sSgOsqnhHD7wQNT2oUQv60B8Znb9YnZ8-0knutkGH2~e4Z2BbGdeKz1fDdLMvDRH5Qq7z7cOo39cjNXVppjEM-sQy8Ip-E6sjuXP10ssTnCpIcydNY1OdJFLY6pxlhTKjmML2hKaJxpyFnXchVYV71lg8us-QTySswmT4qS4nM~NSDj~rJZWRUm9nh9RVTyok~DIhC4maAij9i9zCMmTuQeFJGS6Yyd5eWE5CsPRfs2FBEolE0V7fH01r~MqMAr0hHw7anh-16LqCArEzWyCgA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ"
 filename = 'D:\p\crawler\code.128.mp3'
 r = requests.get(url, stream=True).content
 with open(filename, "wb") as mp3:
-    mp3.write(r)
+    mp3.write(r)'''
