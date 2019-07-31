22ec9187c8e6e7084452e7d57d7492d5082b601c
pengmengnanlan <llanan9817@163.com>
2019-07-31 19:48:42 +0800

    use vimeo

--- connectMysql.py
+++ connectMysql.py
@@ -42,17 +42,19 @@ def getTweets(api, info):
 def createFiles(info):
     now_path = os.getcwd()
     filename = u'' + info + ''
-    image_filename = u'Image'
+    f = now_path + "\\" + filename
+    if os.path.exists(f):
+        shutil.rmtree(f)
+    os.mkdir(f)
+    '''image_filename = u'Image'
     video_filename = u'Original_Video'
     youtube_video_filename = u'Youtube_Video'
     audio_filename = u'Audio'
-    f = now_path + "\\" + filename
     filename_path_photo = now_path + "\\" + filename + "\\" + image_filename
     filename_path_original_video = now_path + "\\" + filename + "\\" + video_filename
     filename_path_youtube_video = now_path + "\\" + filename + "\\" + youtube_video_filename
     filename_path_audio = now_path + "\\" + filename + "\\" + audio_filename
-    if os.path.exists(f):
-        shutil.rmtree(f)
+
     if os.path.exists(filename_path_photo):
         shutil.rmtree(filename_path_photo)
     if os.path.exists(filename_path_original_video):
@@ -64,13 +66,15 @@ def createFiles(info):
     os.makedirs(filename_path_photo)
     os.makedirs(filename_path_original_video)
     os.makedirs(filename_path_youtube_video)
-    os.makedirs(filename_path_audio)
-    return [filename, filename_path_photo, filename_path_original_video, filename_path_youtube_video,
-            filename_path_audio]
+    os.makedirs(filename_path_audio)'''
+    return [filename, f]
 
 
 def isVideo(s):
-    if s.startswith('https://youtu.be/') or s.startswith('https://www.youtube.com/'):
+    # if s.startswith('https://youtu.be/') or s.startswith('https://www.youtube.com/') or s.startswith(
+    #         'https://vimeo.com/'):
+    #     return True
+    if s.startswith('https://vimeo.com/'):
         return True
 
 
@@ -96,22 +100,22 @@ def downloadVideo(video_files, index_of_video, filename_path_video):
 
 def getPhoto(urls_of_photo, filename_path):
     print('Downloading ' + str(len(urls_of_photo)) + ' urls of photo.....')
-    with open(filename_path + '/urls_of_photo_' + info + '.txt', 'w', encoding='utf-8') as txt:
+    with open(filename_path + '/urls_of_photo.txt', 'w', encoding='utf-8') as txt:
         for i in urls_of_photo:
             txt.write(i + '\n')
 
 
-def getVideoFromYouTube(info, urls_of_video, filename_path):
+def getVideo(info, urls_of_video, filename_path):
     print('Downloading ' + str(len(urls_of_video)) + ' urls of video.....')
-    with open(filename_path + '/urls_of_Youtube_video_of_' + info + '.txt', 'w', encoding='utf-8') as txt:
+    with open(filename_path + '/urls_of_video.txt', 'w', encoding='utf-8') as txt:
         for i in urls_of_video:
             txt.write(i + '\n')
-    print(f'getVideoFromYouTube    end：{time.strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f'getVideo    end：{time.strftime("%Y-%m-%d %H:%M:%S")}')
 
 
 def getAudio(info, urls_of_audio, filename_path):
     print('Downloading ' + str(len(urls_of_audio)) + ' urls of audio.....')
-    with open(filename_path + '/urls_of_audio_of_' + info + '.txt', 'w', encoding='utf-8') as txt:
+    with open(filename_path + '/urls_of_audio.txt', 'w', encoding='utf-8') as txt:
         for i in urls_of_audio:
             txt.write(i + '\n')
 
@@ -122,7 +126,7 @@ info = input("Enter twitter user_name - ")
 files = createFiles(info)
 api = Auth()
 all_tweets = getTweets(api, info)
-db = pymysql.connect(host='localhost', user='root', password='1234567', db='download_info', charset='utf8')
+db = pymysql.connect(host='localhost', port=3306, user='root', password='123456', db='download_info', charset='utf8')
 cursor = db.cursor()
 
 image_files = []
@@ -134,12 +138,12 @@ urls_of_audio = set()
 url = ''
 name = ''
 tweet_sql = "insert into tweet_info(user_id, user_name, tweet_id, created_at, message, location) values (%s, %s, %s, %s, %s, %s);"
-media_sql = "insert into media_info(date_info, user_id, user_name, tweet_id, media_type, media_url, media_name, media_store_path) values (%s, %s, %s, %s, %s, %s, %s, %s);"
+media_sql = "insert into media_info(date_info, user_id, user_name, tweet_id, media_type, media_url, media_name, media_store_path, is_stego, is_filter) values (%s, %s, %s, %s, %s, %s, %s, %s, 0, 0);"
 
 try:
     for status in all_tweets:
         message = str(status.full_text.encode('utf-8'))
-        message = re.split('\'',message)[1]
+        message = re.split('\'', message)[1]
 
         try:
             entities = status.extended_entities
@@ -163,7 +167,7 @@ try:
                             url = media[i]['media_url'] + ':large'
                             name = re.split('/', media[i]['media_url'])[4]
                             image_files.append(url)
-                            path = files[1] + '\\' + name
+                            path = files[1] + '\\'
                         elif media_type == 'video':
                             video_url = []
                             type_flag = 2
@@ -174,7 +178,7 @@ try:
                             name = status.id_str + ".mp4"
                             video_files.append(url)
                             index_of_video.append(status.id_str)
-                            path = files[2] + '\\' + status.id_str + '.mp4'
+                            path = files[1] + '\\'
 
                         cursor.execute(media_sql,
                                        [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
@@ -192,18 +196,22 @@ try:
         try:
             for u in status.entities['urls']:
                 if isVideo(u['expanded_url']):
-                    type_flag = 3
+                    type_flag = 2
                     cursor.execute(tweet_sql,
                                    [status.user.id_str, status.user.screen_name, status.id_str, status.created_at,
                                     message, status.user.location])
                     db.commit()
                     url = u['expanded_url']
-                    if re.match('https://youtu.be/', url):
+                    if re.match('https://vimeo.com/', url):
                         name = re.split('/', url)[-1] + '.mp4'
-                    elif re.match('https://www.youtube.com/', url):
-                        s = re.split('v=', url)
-                        name = re.split('&', s[-1])[0] + '.mp4'
-                    path = files[3] + '\\' + name
+                    # if re.match('https://youtu.be/', url):
+                    #     name = re.split('/', url)[-1] + '.mp4'
+                    # elif re.match('https://www.youtube.com/', url):
+                    #     s = re.split('v=', url)
+                    #     name = re.split('&', s[-1])[0] + '.mp4'
+                    # elif re.match('https://vimeo.com/', url):
+                    #     name = re.split('/', url)[-1] + '.mp4'
+                    path = files[1] + '\\'
                     urls_of_video.add(u['expanded_url'])
                     cursor.execute(media_sql,
                                    [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
@@ -211,14 +219,14 @@ try:
                     db.commit()
 
                 if isAudio(u['expanded_url']):
-                    type_flag = 4
+                    type_flag = 3
                     cursor.execute(tweet_sql,
                                    [status.user.id_str, status.user.screen_name, status.id_str, status.created_at,
                                     status.full_text.encode('utf-8'), status.user.location])
                     db.commit()
                     url = u['expanded_url']
-                    name = re.split('/',url)[3]+'.mp3'
-                    path = files[4] + '\\' + name
+                    name = re.split('/', url)[3] + '.mp3'
+                    path = files[1] + '\\'
                     urls_of_audio.add(u['expanded_url'])
                     cursor.execute(media_sql,
                                    [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
@@ -234,12 +242,12 @@ except tweepy.TweepError as e:
 # Multiple-threading
 if __name__ == '__main__':
     thread = []
-    t1 = threading.Thread(target=getPhoto, args=(image_files, files[1]))
-    t2 = threading.Thread(target=downloadVideo, args=(video_files, index_of_video, files[2]))
-    t3 = threading.Thread(target=getVideoFromYouTube, args=(info, urls_of_video, files[3]))
-    t4 = threading.Thread(target=getAudio, args=(info, urls_of_audio, files[4]))
+    t1 = threading.Thread(target=getPhoto, args=(image_files, files[0]))
+    # t2 = threading.Thread(target=downloadVideo, args=(video_files, index_of_video, files[0]))
+    t3 = threading.Thread(target=getVideo, args=(info, urls_of_video, files[0]))
+    t4 = threading.Thread(target=getAudio, args=(info, urls_of_audio, files[0]))
     thread.append(t1)
-    thread.append(t2)
+    # thread.append(t2)
     thread.append(t3)
     thread.append(t4)
 
@@ -248,3 +256,19 @@ if __name__ == '__main__':
     for t in thread:
         t.join()
     print(f'End：{time.strftime("%Y-%m-%d %H:%M:%S")}')
+    os.system(
+        'cd "C:/Users/alina/AppData/Local/Programs/Python/Python37/Scripts/Scripts" '
+        '& activate & cd "' + files[1] + '" '
+                                         '& youtube-dl -c -i --id --no-playlist -a urls_of_audio.txt'
+                                         '& youtube-dl -c -i --id --no-playlist -a urls_of_video.txt'
+                                         '& youtube-dl -c -i --id --no-playlist -a urls_of_photo.txt')
+    try:
+        images = os.listdir(files[1])
+        for filename in images:
+            portion = os.path.splitext(filename)
+            if portion[1] == ".unknown_video":
+                new_name = portion[0] + ".jpg"
+                os.chdir(files[1])
+                os.rename(filename, new_name)
+    except KeyError:
+        pass
