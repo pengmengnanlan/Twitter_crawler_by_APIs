297a2e88c6079cb16982828c3b172cf931cd34ea
pengmengnanlan <llanan9817@163.com>
2019-08-01 16:01:26 +0800

    use flickr to get photo

--- connectMysql.py
+++ connectMysql.py
@@ -8,6 +8,7 @@ import requests
 import shutil
 import pymysql
 from tweepy import OAuthHandler
+from bs4 import BeautifulSoup
 
 
 def Auth():
@@ -28,7 +29,7 @@ def Auth():
 def getTweets(api, info):
     all_tweets = []
     # search tweets with the screen_name of user
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name=info, tweet_mode='extended').items():
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name=info, tweet_mode='extended').items(3):
         all_tweets.append(tweet)
 
     # search tweets with the keywords
@@ -70,6 +71,11 @@ def createFiles(info):
     return [filename, f]
 
 
+def isPhoto(s):
+    if s.startswith('https://flic.kr/p/'):
+        return True
+
+
 def isVideo(s):
     # if s.startswith('https://youtu.be/') or s.startswith('https://www.youtube.com/') or s.startswith(
     #         'https://vimeo.com/'):
@@ -105,7 +111,7 @@ def getPhoto(urls_of_photo, filename_path):
             txt.write(i + '\n')
 
 
-def getVideo(info, urls_of_video, filename_path):
+def getVideo(urls_of_video, filename_path):
     print('Downloading ' + str(len(urls_of_video)) + ' urls of video.....')
     with open(filename_path + '/urls_of_video.txt', 'w', encoding='utf-8') as txt:
         for i in urls_of_video:
@@ -113,13 +119,38 @@ def getVideo(info, urls_of_video, filename_path):
     print(f'getVideo    end：{time.strftime("%Y-%m-%d %H:%M:%S")}')
 
 
-def getAudio(info, urls_of_audio, filename_path):
+def getAudio(urls_of_audio, filename_path):
     print('Downloading ' + str(len(urls_of_audio)) + ' urls of audio.....')
     with open(filename_path + '/urls_of_audio.txt', 'w', encoding='utf-8') as txt:
         for i in urls_of_audio:
             txt.write(i + '\n')
 
     print(f'getAudio    end：{time.strftime("%Y-%m-%d %H:%M:%S")}')
+
+
+def getHTMLText(url):
+    try:
+        res = requests.get(url, timeout=6)
+        res.raise_for_status()
+        res.encoding = res.apparent_encoding
+        return res.text
+    except:
+        return 'error'
+
+
+def getPhotoUrls(url):
+    demo = getHTMLText(url)
+    soup = BeautifulSoup(demo, 'html.parser')
+    tag = ''
+    for t in soup.find_all(name='script'):
+        tag += str(t)
+    tag = re.findall(r'live.staticflickr.com.+._o+\.jpg', tag)
+    result = re.split('\\\\', (re.split(':"', tag[0])[-1]))
+    photo_url = ''
+    for s in result:
+        if s:
+            photo_url += s
+    return 'https:' + photo_url
 
 
 info = input("Enter twitter user_name - ")
@@ -135,58 +166,59 @@ video_url = []
 index_of_video = []
 urls_of_video = set()
 urls_of_audio = set()
+urls_of_photo = set()
 url = ''
 name = ''
 tweet_sql = "insert into tweet_info(user_id, user_name, tweet_id, created_at, message, location) values (%s, %s, %s, %s, %s, %s);"
 media_sql = "insert into media_info(date_info, user_id, user_name, tweet_id, media_type, media_url, media_name, media_store_path, is_stego, is_filter) values (%s, %s, %s, %s, %s, %s, %s, %s, 0, 0);"
 
-try:
-    for status in all_tweets:
-        message = str(status.full_text.encode('utf-8'))
-        message = re.split('\'', message)[1]
-
-        try:
-            entities = status.extended_entities
-        except AttributeError:
-            entities = status.entities
-        if entities:
-            media = entities.get('media', [])
-
-            if len(media) != 0:
-                cursor.execute(tweet_sql,
-                               [status.user.id_str, status.user.screen_name, status.id_str, status.created_at,
-                                message, status.user.location])
-                db.commit()
-
-                for i in range(len(media)):
-                    media_type = media[i]['type']
-
-                    if media_type == 'photo' or media_type == 'video':
-                        if media_type == 'photo':
-                            type_flag = 1
-                            url = media[i]['media_url'] + ':large'
-                            name = re.split('/', media[i]['media_url'])[4]
-                            image_files.append(url)
-                            path = files[1] + '\\'
-                        elif media_type == 'video':
-                            video_url = []
-                            type_flag = 2
-                            for v in media[i]['video_info']['variants']:
-                                if v['content_type'] == 'video/mp4':
-                                    video_url.append(v['url'])
-                            url = video_url[0]
-                            name = status.id_str + ".mp4"
-                            video_files.append(url)
-                            index_of_video.append(status.id_str)
-                            path = files[1] + '\\'
-
-                        cursor.execute(media_sql,
-                                       [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
-                                        status.id_str, type_flag, url, name, path])
-                        db.commit()
-except tweepy.TweepError as e:
-    print(e.reason)
-    time.sleep(60)
+# try:
+#     for status in all_tweets:
+#         message = str(status.full_text.encode('utf-8'))
+#         message = re.split('\'', message)[1]
+#
+#         try:
+#             entities = status.extended_entities
+#         except AttributeError:
+#             entities = status.entities
+#         if entities:
+#             media = entities.get('media', [])
+#
+#             if len(media) != 0:
+#                 cursor.execute(tweet_sql,
+#                                [status.user.id_str, status.user.screen_name, status.id_str, status.created_at,
+#                                 message, status.user.location])
+#                 db.commit()
+#
+#                 for i in range(len(media)):
+#                     media_type = media[i]['type']
+#
+#                     if media_type == 'photo' or media_type == 'video':
+#                         if media_type == 'photo':
+#                             type_flag = 1
+#                             url = media[i]['media_url'] + ':large'
+#                             name = re.split('/', media[i]['media_url'])[4]
+#                             image_files.append(url)
+#                             path = files[1] + '\\'
+#                         elif media_type == 'video':
+#                             video_url = []
+#                             type_flag = 2
+#                             for v in media[i]['video_info']['variants']:
+#                                 if v['content_type'] == 'video/mp4':
+#                                     video_url.append(v['url'])
+#                             url = video_url[0]
+#                             name = status.id_str + ".mp4"
+#                             video_files.append(url)
+#                             index_of_video.append(status.id_str)
+#                             path = files[1] + '\\'
+#
+#                         cursor.execute(media_sql,
+#                                        [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
+#                                         status.id_str, type_flag, url, name, path])
+#                         db.commit()
+# except tweepy.TweepError as e:
+#     print(e.reason)
+#     time.sleep(60)
 
 try:
     for status in all_tweets:
@@ -195,6 +227,21 @@ try:
 
         try:
             for u in status.entities['urls']:
+                if isPhoto(u['expanded_url']):
+                    type_flag = 1
+                    cursor.execute(tweet_sql,
+                                   [status.user.id_str, status.user.screen_name, status.id_str, status.created_at,
+                                    message, status.user.location])
+                    db.commit()
+                    url = getPhotoUrls(u['expanded_url'])
+                    name = re.split('/', url)[-1]
+                    path = files[1]+'\\'
+                    urls_of_photo.add(url)
+                    cursor.execute(media_sql,
+                                   [time.strftime("%Y-%m-%d %H:%M:%S"), status.user.id_str, status.user.screen_name,
+                                    status.id_str, type_flag, u['expanded_url'], name, path])
+                    db.commit()
+
                 if isVideo(u['expanded_url']):
                     type_flag = 2
                     cursor.execute(tweet_sql,
@@ -202,15 +249,12 @@ try:
                                     message, status.user.location])
                     db.commit()
                     url = u['expanded_url']
-                    if re.match('https://vimeo.com/', url):
-                        name = re.split('/', url)[-1] + '.mp4'
+                    name = re.split('/', url)[-1] + '.mp4'
                     # if re.match('https://youtu.be/', url):
                     #     name = re.split('/', url)[-1] + '.mp4'
                     # elif re.match('https://www.youtube.com/', url):
                     #     s = re.split('v=', url)
                     #     name = re.split('&', s[-1])[0] + '.mp4'
-                    # elif re.match('https://vimeo.com/', url):
-                    #     name = re.split('/', url)[-1] + '.mp4'
                     path = files[1] + '\\'
                     urls_of_video.add(u['expanded_url'])
                     cursor.execute(media_sql,
@@ -233,6 +277,7 @@ try:
                                     status.id_str, type_flag, url, name, path])
                     db.commit()
         except IndexError:
+            urls_of_photo.add('')
             urls_of_video.add('')
             urls_of_audio.add('')
 except tweepy.TweepError as e:
@@ -242,10 +287,10 @@ except tweepy.TweepError as e:
 # Multiple-threading
 if __name__ == '__main__':
     thread = []
-    t1 = threading.Thread(target=getPhoto, args=(image_files, files[0]))
+    t1 = threading.Thread(target=getPhoto, args=(urls_of_photo, files[0]))
     # t2 = threading.Thread(target=downloadVideo, args=(video_files, index_of_video, files[0]))
-    t3 = threading.Thread(target=getVideo, args=(info, urls_of_video, files[0]))
-    t4 = threading.Thread(target=getAudio, args=(info, urls_of_audio, files[0]))
+    t3 = threading.Thread(target=getVideo, args=(urls_of_video, files[0]))
+    t4 = threading.Thread(target=getAudio, args=(urls_of_audio, files[0]))
     thread.append(t1)
     # thread.append(t2)
     thread.append(t3)
@@ -262,13 +307,13 @@ if __name__ == '__main__':
                                          '& youtube-dl -c -i --id --no-playlist -a urls_of_audio.txt'
                                          '& youtube-dl -c -i --id --no-playlist -a urls_of_video.txt'
                                          '& youtube-dl -c -i --id --no-playlist -a urls_of_photo.txt')
-    try:
-        images = os.listdir(files[1])
-        for filename in images:
-            portion = os.path.splitext(filename)
-            if portion[1] == ".unknown_video":
-                new_name = portion[0] + ".jpg"
-                os.chdir(files[1])
-                os.rename(filename, new_name)
-    except KeyError:
-        pass
+    # try:
+    #     images = os.listdir(files[1])
+    #     for filename in images:
+    #         portion = os.path.splitext(filename)
+    #         if portion[1] == ".unknown_video":
+    #             new_name = portion[0] + ".jpg"
+    #             os.chdir(files[1])
+    #             os.rename(filename, new_name)
+    # except KeyError:
+    #     pass
